dbe27c587366d1237f564d2594ea25b9eb870aab
Aayush Gupta <guptaaayush432@gmail.com>
2023-02-08 15:28:47 +0530

    Updated hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,29 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@openzeppelin/hardhat-upgrades");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL;
+const MUMBAI_PRIVATE_KEY = process.env.MUMBAI_PRIVATE_KEY;
+const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY;
+
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.10",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    mumbai: {
+      url: MUMBAI_RPC_URL,
+      accounts: [MUMBAI_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: POLYGONSCAN_API_KEY,
+    },
+  },
 };
