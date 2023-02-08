252674d36ab93bf91c90b9016946474246259588
Aayush Gupta <guptaaayush432@gmail.com>
2023-02-08 17:39:20 +0530

    Update Box.sol

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -1,10 +1,14 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.10;
 
 contract Box {
     uint public val;
 
-    function initilaize(uint _val) external {
+    // constructor(uint _val) {
+    //     val = _val;
+    // }
+
+    function initialize(uint _val) external {
         val = _val;
     }
 }
